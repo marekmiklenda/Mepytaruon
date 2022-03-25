aaa0218336a6e2247392b23ff534b9f646999c19
Marek Miklenda <marekmiklenda@gmail.com>
2022-03-25 09:11:29 +0100

    Catching errors from nonexistent/incompatible file types Correct use of stderr

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,10 +2,30 @@ import com.xenomachina.argparser.ArgParser
 import com.xenomachina.argparser.mainBody
 import miklenda.marek.mepytaruon.control.Environment
 import miklenda.marek.mepytaruon.control.Interpreter
+import java.io.File
+import javax.imageio.IIOException
+import kotlin.system.exitProcess
 
 internal fun main(args: Array<String>) = mainBody {
     ArgParser(args).parseInto(::MyArgs).run {
-        val exit = Interpreter.run(program, Environment(x, y, direction, cells, trace))
-        println("\nProcess finished with exit code $exit")
+        val file = File(program)
+        if (!file.exists()) {
+            System.err.println("The file specified doesn't exist!")
+            return@mainBody
+        }
+
+        val type = file.toURL().openConnection().contentType
+        if (type == "image/jpeg") System.err.println("Warning: image/jpeg files may not be executed correctly. Consider using image/png instead.\n")
+
+        try {
+            val exit = Interpreter.run(program, Environment(x, y, direction, cells, trace))
+            exitProcess(exit)
+        } catch (e: Exception) {
+            when (e) {
+                is NullPointerException,
+                is IIOException -> System.err.println("Invalid file type: $type") //${Regex("\\..+$").find(program)?.value}
+                else -> System.err.println("An unknown error has occurred.\n${e.stackTraceToString()}")
+            }
+        }
     }
 }
